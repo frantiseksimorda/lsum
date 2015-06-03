e33f3781927ddfdfa7c4f091789f910e5d59a39a
karlosss <los.karlosss@gmail.com>
2015-06-03 23:04:30 +0200

    password reader done

--- scripts/read_pwds.py
+++ scripts/read_pwds.py
@@ -1,6 +1,41 @@
+# coding: utf8
+
+# PASSWORD READER
+# author: Karel Jilek
+#
+# Script to retrieve passwords from the 'passwords.pwd' file.
+#
+# USAGE:
+# password = Password().read("password_description")
+# for example: "SafeQ"
+#
+# PASSWORDS.PWD SYNTAX:
+# my_very_secret_password   #password_description
+# for example: baka123   #bakalari
+#
+# DO NOT WRITE ANY PASSWORDS BELOW THE "EOF" LINE, all the lines below are ignored
+
 from os.path import dirname
 
-f = open(dirname(__file__) + '/../hesla.pwd')
+class Password:
 
-for i in f:
-    print i
+    def __init__(self):
+
+        f = open(dirname(__file__) + '/../passwords.pwd')
+        self.pwds = {}
+
+        for line in f:
+            try:
+                if line.rstrip() == "EOF":
+                    break
+                x = line.split("#")
+                self.pwds[x[1].rstrip()] = x[0].rstrip()
+            except:
+                raise Exception("The file 'passwords.pwd' is corrupted")
+
+    def read(self, pwd):
+
+        if not pwd in self.pwds.keys():
+            raise Exception("Cannot find reference of '"+pwd+"' password in 'passwords.pwd'")
+
+        return self.pwds[pwd]
