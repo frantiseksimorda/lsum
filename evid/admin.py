2c569ddca41a56a0986b1ca8f0ed2d0836066b94
Karel Jílek <los.karlosss@gmail.com>
2015-06-19 14:31:31 +0200

    student synchronization

--- evid/admin.py
+++ evid/admin.py
@@ -1,24 +1,24 @@
 from django.contrib import admin
 from models import Student, Teacher, School_class
 
-class studentAdmin(admin.ModelAdmin):
+class StudentAdmin(admin.ModelAdmin):
     list_display = ("id", "surname", "name", "school_class")
     list_display_links = ('surname', )
     search_fields = ('surname',)
 
 
-admin.site.register(Student, studentAdmin)
+admin.site.register(Student, StudentAdmin)
 
-class teacherAdmin(admin.ModelAdmin):
+class TeacherAdmin(admin.ModelAdmin):
     list_display = ("surname", "name")
 
-admin.site.register(Teacher, teacherAdmin)
+admin.site.register(Teacher, TeacherAdmin)
 
 
-class schoolClassAdmin(admin.ModelAdmin):
-    list_display = ("short_name", "id" )
+class SchoolClassAdmin(admin.ModelAdmin):
+    list_display = ("short_name", "id")
 
-admin.site.register(School_class, schoolClassAdmin)
+admin.site.register(School_class, SchoolClassAdmin)
 
 
 
