6c73234266f3bc2374b2ff178237fe48b1f55dcf
Frantisek Simorda <frantisek.simorda@gmail.com>
2015-06-26 15:42:24 +0200

    Generate login func bind to button

--- evid/views.py
+++ evid/views.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
-# from django.shortcuts import render
-from django.shortcuts import render_to_response
-from django.template import RequestContext
 from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
+from evid.models import Student, School_class, User_account_student
+from connection import Connection
+from misc import stringList
+from random import randint
+from hashlib import sha512
+from unicodedata import normalize
 
 
 def baka_sync(request):
     """ Sync users with Bakalari MSSQL database, add new studenst from new classes"""
-    from evid.models import Student, School_class
-    from connection import Connection
-
     # connection and query to the source database
     conn = Connection("bakalari")
     data = conn.execute("SELECT trida FROM zaci")
@@ -81,4 +80,78 @@ def maturanti_lock(request):
 
 def generate_logins(request):
     """ after sync with bakalari (imported new users), generate new usernames and password for new classes """
-    return request
+
+    baka_codes = stringList(Student.objects.values_list("kod_baka"))
+    login_codes = stringList(User_account_student.objects.values_list("kod_baka"))
+    existing_logins = stringList(User_account_student.objects.values_list("login"))
+
+
+    def shorten(x, length):
+        # first chars of string x, normalized, lowercase; if shorter than 3, automatically extended by xs from the right
+        x = x[:3].rstrip()
+        x = normalize('NFKD', x).encode('ascii', 'ignore')
+        x = x.lower()
+
+        while len(x) < 3:
+            x += "x"
+
+        return x
+
+
+    def generate_login(name, surname):
+
+        surname = shorten(surname, 3)
+        name = shorten(name, 2)
+        username = "x" + surname + name
+
+        counter = 1
+
+        for existing_login in existing_logins:
+            if username in existing_login:
+                counter += 1
+
+        number = "0" + str(counter) if counter < 10 else str(counter)
+
+        username += number
+
+        return username
+
+    def generate_password():
+        # takes a random seq. of a SHA512 hash of a random integer in range from 0 to 2bil.
+        random_hash = ""
+
+        while len(random_hash) < 8:
+            random_hash = sha512(str(randint(0, 2000000000))).hexdigest()
+
+        random_offset = randint(0, len(random_hash)-8)
+
+        def_pwd = random_hash[random_offset:random_offset+8]
+
+        return def_pwd
+
+
+    # write user accounts into DB
+    for code in baka_codes:
+        if code not in login_codes:
+
+            # get name and surname of the student and generate username and password
+            surname = Student.objects.values_list("surname").filter(kod_baka=code)[0][0]
+            name = Student.objects.values_list("name").filter(kod_baka=code)[0][0]
+
+            login = generate_login(name, surname)
+            password = generate_password()
+
+            # generate email
+            email = login + "@gjk.cz"
+
+            account = User_account_student(
+                kod_baka=code,
+                login=login,
+                default_passwd=password,
+                email=email,
+            )
+
+            account.save()
+            existing_logins.append(login)
+
+    return HttpResponseRedirect("/admin/evid/user_account_student/")
