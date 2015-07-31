6351ae411b16e5e4cbb179711dad381e343e41df
Frantisek Simorda <frantisek.simorda@gmail.com>
2015-07-31 12:55:35 +0200

    adding users script

--- add_unix_user.py
+++ add_unix_user.py
@@ -19,20 +19,46 @@ def is_generated(user):
 
 def generate(username=""):
     """ Generovani uzvatelu Unixserveru a Sambe """
-    # Script.object.values_list('command')
     users_for_process = Script.objects.values_list()
+    for user in Script.objects.all():
+        # print (user)
+        if not user.timestamp_executed:
+            if user.action == "add":
 
-    process = Popen(["useradd","-m", "-G" ], stdout=PIPE, stderr=PIPE)
-    stdout, stderr = process.communicate()
-    script = Script(
-        timestamp_executed = datetime.now(),
-        stdout = stdout,
-        stderr = stderr,
-        result_code = 1 if not stderr else 0
-                    )
-    script.save()
+                if user.user_type == "student":
+                    user_group = "505"
+                    user_homedir = "/user/studenti/" + user.login
+                elif user.user_type == "kantor":
+                    user_group = "555"
+                    user_homedir = "/user/kantori/" + user.login
+                fullname = user.name + " " + user.surname
 
-for user in Script.objects.values_list():
-    print (user)
+                # useradd process
+                useradd_proc = Popen(["useradd", "-s /bin/false", "-m", "-d", user_homedir, "-c", fullname, "-g", user_group, user.login], stdout=PIPE, stderr=PIPE)
+                stdout, stderr = useradd_proc.communicate()
 
-# generate('fanda')
+                # password sett process
+                smbpasswd_proc = Popen(["smbpasswd", "-a", user.login, ], stdout=PIPE, stderr=PIPE)
+                stdout1, stderr1 = smbpasswd_proc.communicate()
+
+                # create WWW dir and correct rights
+                create_www_proc = Popen(["mkdir", user_homedir + "WWW"], stdout=PIPE, stderr=PIPE)
+                stdout2, stderr2 = create_www_proc.communicate()
+
+                # fix owner and group of WWW dir
+                chown_www_proc = Popen(["chown", user.login + ":nobody", user_homedir + "WWW"], stdout=PIPE, stderr=PIPE)
+                stdout3, stderr3 = chown_www_proc.communicate()
+
+                # fix owner and group of WWW dir
+                chmod_www_proc = Popen(["chmod", "750"], stdout=PIPE, stderr=PIPE)
+                stdout4, stderr4 = chmod_www_proc.communicate()
+
+                Script.objects.filter(login=user.login).update(
+                    timestamp_executed = datetime.now(),
+                    stdout = stdout,
+                    stderr = stderr,
+                    result_code = 1 if not stderr else 0
+                        )
+
+
+generate('fanda')
